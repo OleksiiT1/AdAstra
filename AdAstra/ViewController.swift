5698e31c6de0137b87254d85ab94293982bddb40
Oleksii Tiurenkov <alterego4@gmail.com>
2019-04-14 21:50:01 +0300

    Initial Commit

---
+++ AdAstra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AdAstra
+//
+//  Created by Aleksey Tyurenkov on 4/14/19.
+//  Copyright © 2019 com.ovt. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
